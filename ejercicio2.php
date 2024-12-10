0885d38d21adedf0bfd07d6981f0f5956c827807
Dellos7 <david.lopez.castellote@gmail.com>
2024-12-10 10:43:46 +0100

    actualizado ejercicio 2 y creado ejercicio 3

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,7 +1,11 @@
 <?php
-
-    if (isset($_POST['slider_temp']){
-        echo "Temperatura: " . $_POST['slider_temp']
+    if (isset($_POST['slider_temp'])){
+        $celsius = floatval($_POST['slider_temp']);
+        echo "Temperatura (ºC): " . $_POST['slider_temp'] . "<br>";
+        $kelvin = $celsius + 273.15;
+        echo "Temperatura (ºK): " . $kelvin . "<br>";
+        $fahrenheit = ($celsius * 9 / 5) + 32;
+        echo "Temperatura (ºF): " . $fahrenheit;
     }
 
 ?>
